6d91d96568a653432d38081867952f92736a4d2e
lujiang <lujiang0111@gmail.com>
2021-01-13 13:42:53 +0800

    修改内容： 1.添加extern C

--- include/PlatformBaseApi.h
+++ include/PlatformBaseApi.h
@@ -4,6 +4,11 @@
 #include <stdint.h>
 #include <stdarg.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #if defined(WIN32) || defined(_WINDLL)
 #if defined(PLATFORM_BASE_EXPORT)
 #define PLATFORM_BASE_API	__declspec(dllexport)
@@ -14,18 +19,22 @@
 #define PLATFORM_BASE_API
 #endif
 
-typedef enum PlatformLogLevel_
-{
-	PL_LEVEL_DEBUG = 0,
-	PL_LEVEL_INFO,
-	PL_LEVEL_WARNING,
-	PL_LEVEL_ERROR,
-	PL_LEVEL_FATAL,
-}EPlatformLogLevel;
+	typedef enum PlatformLogLevel_
+	{
+		PL_LEVEL_DEBUG = 0,
+		PL_LEVEL_INFO,
+		PL_LEVEL_WARNING,
+		PL_LEVEL_ERROR,
+		PL_LEVEL_FATAL,
+	}EPlatformLogLevel;
 
-/*****************************
-设置PlatformBase库内日志输出回调
-*****************************/
-PLATFORM_BASE_API void SetPlatformBaseLogSetCallback(void(*cb)(EPlatformLogLevel level, const char *fmt, va_list vl));
+	/*****************************
+	设置PlatformBase库内日志输出回调
+	*****************************/
+	PLATFORM_BASE_API void SetPlatformBaseLogSetCallback(void(*cb)(EPlatformLogLevel level, const char *fmt, va_list vl));
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // !PLATFORM_BASE_API_H_
