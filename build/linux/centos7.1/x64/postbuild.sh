b10979769af54aba68b5f86b4fccd1ae24b572da
lujiang <Lujiang0111@gmail.com>
2021-02-26 15:29:12 +0800

    添加release编译选项

--- build/linux/centos7.1/x64/postbuild.sh
+++ build/linux/centos7.1/x64/postbuild.sh
@@ -2,8 +2,13 @@ base_path=$1
 project=$2
 version=$3
 target=$4
+
+if [ $# -ge 5 ]; then
+dir=${base_path}/${project}/v${version}/linux/centos7.1/x64_release
+else
 dir=${base_path}/${project}/v${version}/linux/centos7.1/x64
-rm -rf ${dir}
+fi
+
 mkdir -p ${dir}/include/
 mkdir -p ${dir}/lib/
 rm -rf ${dir}/include/*
